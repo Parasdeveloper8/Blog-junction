940248bbea3c2d53ce042712c0be4e200b3e75e9
Parasdeveloper8 <parasprajapat8th@gmail.com>
2025-04-03 23:11:32 +0530

    [update]:Created api to register user

---
+++ backend/server/app/Http/Controllers/Authentication.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+class Authentication extends Controller
+{
+   function registerController(Request $req){
+        try{
+         //apply validations
+        $req->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|email',
+            'password' => 'required|min:8'
+        ]);
+        
+         //Add values in User model's register function
+         User::register($req->name,$req->email,$req->password);
+         return response()->json([
+            "success" => true,
+            "info" => "User registered successfully"
+         ], 201);
+
+        }catch(\Exception $e){
+           return response()->json([
+            "success" => false,
+            "info" => $e->getMessage()
+         ], 500);
+        }
+   }
+}
