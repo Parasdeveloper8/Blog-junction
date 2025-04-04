3465a2026ab434161f7648da6ab8a8d8f32d6876
Parasdeveloper8 <parasprajapat8th@gmail.com>
2025-04-04 21:41:57 +0530

    [update]:Registration functionality completed

--- backend/server/routes/api.php
+++ backend/server/routes/api.php
@@ -4,4 +4,4 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Authentication;
 
-Route::get('/register', [Authentication::class,'registerController']);
+Route::post('/register', [Authentication::class,'registerController']);
