c94a55221225830b542ee4492682a3eeb296e5e0
Parasdeveloper8 <parasprajapat8th@gmail.com>
2025-04-08 17:43:04 +0530

    [update]:Showing blogs on homepage completed

--- frontend/UI/src/components/main.tsx
+++ frontend/UI/src/components/main.tsx
@@ -1,8 +1,49 @@
+import axios, { AxiosResponse} from 'axios';
+import { useEffect, useState } from 'react';
+import Loader from './loader';
 const Main = () =>{
+    interface ApiResponse {
+        email:string;
+        id: number;
+        text: string;
+        created_at: string;
+        updated_at: string;
+     }
+
+     const [data,setData] = useState<null | ApiResponse []>(null);
+
+     useEffect(()=>{
+        async function fetch(){
+        const response:AxiosResponse<{info : ApiResponse[] }| null>
+         = 
+         await axios.get('http://localhost:8000/api/posts'
+           ,{
+              headers: {
+                 Authorization: `Bearer ${localStorage.getItem('token')}`
+               }
+           }
+        );
+        if(response.data)
+        setData(response.data.info);
+        }
+        fetch();
+       },[]);
+
     return (
         <>
         <div>
-            <p>Loading..</p>
+        {data ? 
+         data.map((dt) => {
+            return(
+            <div className="card mx-auto mt-2" style={{width: "36rem"}}>
+               <div className="card-body" key={dt.id}>
+                 <h5 className="card-title">{dt.email}</h5>
+                 <p className="card-text">{dt.text}</p>
+               </div>
+            </div>
+            );
+         }) : <Loader/>
+      }
         </div>
         </>
     )
